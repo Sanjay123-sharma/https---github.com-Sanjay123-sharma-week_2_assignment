da60b81ebdba24221e451ef460f4585e53a46664
Sanjay123-sharma <sanjukumar595857@gmail.com>
2025-05-03 16:33:36 +0530

    commit

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -6,7 +6,7 @@ const Task=({task,onToggle,onDelete})=>{
         <div className={`task  ${task.completed ? 'completed' : 'incomplete'}`}>
             <h3>{task.name}</h3>
             <p>{task.description}</p>
-            <button onClick={() => onToggle(task.id)} className="`btn btn-sm ${task.completed ? 'btn-warning' : 'btn-success'}">
+            <button onClick={() => onToggle(task.id)} className={`btn btn-sm ${task.completed ? 'btn-warning' : 'btn-success'}`}>
         {task.completed ? 'Undo' : 'Complete'}
       </button>
       <button onClick={() => onDelete(task.id)}>Delete</button>
