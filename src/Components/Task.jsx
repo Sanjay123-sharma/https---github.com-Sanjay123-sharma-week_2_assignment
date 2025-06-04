f93436225f5457ab8e0ad4663ebc645850b2f38f
Sanjay123-sharma <sanjukumar595857@gmail.com>
2025-06-04 13:13:31 +0530

    commit

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -18,25 +18,4 @@ const Task = ({ task, onToggle, onDelete }) => {
   );
 };
 
-<<<<<<< HEAD
 export default Task;
-=======
-
-const Task=({task,onToggle,onDelete})=>{
-    return(
-        <>
-        <div className={`task  ${task.completed ? 'completed' : 'incomplete'}`}>
-            <h3>{task.name}</h3>
-            <p>{task.description}</p>
-            <button onClick={() => onToggle(task.id)} className={`btn btn-sm ${task.completed ? 'btn-warning' : 'btn-success'}`}>
-        {task.completed ? 'Undo' : 'Complete'}
-      </button>
-      <button onClick={() => onDelete(task.id)}>Delete</button>
-
-        </div>
-        </>
-    )
-}
-
-export default Task
->>>>>>> b1cb9cb52f1eea50d893367fac69f768a6a4f6e8
